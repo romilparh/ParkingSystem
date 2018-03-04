4021ad2c5ded00cf2be5d41a61634286955c8189
Romil <romilparhwal007@gmail.com>
2018-03-04 10:38:10 -0500

    Add Home Screen View Controller

--- ParkingSystemBugPACers/HomeScreenViewController.swift
+++ ParkingSystemBugPACers/HomeScreenViewController.swift
@@ -12,10 +12,17 @@ class HomeScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+      //  var image = UIImage(named: "Navigation.png")
+      //  image = image?.withRenderingMode(.alwaysOriginal)
+      //  self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style:.plain, target: nil, action: nil)
+        let newBtn = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: nil)
+        self.navigationItem.leftBarButtonItem = newBtn //self.navigationItem.leftBarButtonItems = [newBtn,anotherBtn]
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
