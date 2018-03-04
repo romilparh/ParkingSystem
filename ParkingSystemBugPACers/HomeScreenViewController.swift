bbfcb252fa12b45a249fdceca35a7a389b73b84a
Romil <romilparhwal007@gmail.com>
2018-03-04 12:05:27 -0500

    Add Working Side Bar View by implementing View on View on click of Button

--- ParkingSystemBugPACers/HomeScreenViewController.swift
+++ ParkingSystemBugPACers/HomeScreenViewController.swift
@@ -10,8 +10,26 @@ import UIKit
 
 class HomeScreenViewController: UIViewController {
 
+    var sidebarView: SidebarView!
+    var blackScreen: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let newBtn = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: #selector(btnMenuAction))
+        self.navigationItem.leftBarButtonItem = newBtn 
+        sidebarView = SidebarView(frame: CGRect(x: 0, y: 0, width: 0, height: self.view.frame.height))
+        sidebarView.delegate = self
+        sidebarView.layer.zPosition=100
+        self.view.isUserInteractionEnabled=true
+        self.navigationController?.view.addSubview(sidebarView)
+        
+        blackScreen=UIView(frame: self.view.bounds)
+        blackScreen.backgroundColor=UIColor(white: 0, alpha: 0.3)
+        blackScreen.isHidden=true
+        self.navigationController?.view.addSubview(blackScreen)
+        blackScreen.layer.zPosition=99
+        let tapGestRecognizer = UITapGestureRecognizer(target: self, action: #selector(blackScreenTapAction(sender:)))
+        blackScreen.addGestureRecognizer(tapGestRecognizer)
         // Do any additional setup after loading the view.
     }
 
@@ -19,24 +37,50 @@ class HomeScreenViewController: UIViewController {
       //  var image = UIImage(named: "Navigation.png")
       //  image = image?.withRenderingMode(.alwaysOriginal)
       //  self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style:.plain, target: nil, action: nil)
-        let newBtn = UIBarButtonItem(title: "Menu", style: .plain, target: self, action: nil)
-        self.navigationItem.leftBarButtonItem = newBtn //self.navigationItem.leftBarButtonItems = [newBtn,anotherBtn]
+        }
+    
+    @objc func btnMenuAction() {
+        blackScreen.isHidden=false
+        UIView.animate(withDuration: 0.3, animations: {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 250, height: self.sidebarView.frame.height)
+        }) { (complete) in
+            self.blackScreen.frame=CGRect(x: self.sidebarView.frame.width, y: 0, width: self.view.frame.width-self.sidebarView.frame.width, height: self.view.bounds.height+100)
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func blackScreenTapAction(sender: UITapGestureRecognizer) {
+        blackScreen.isHidden=true
+        blackScreen.frame=self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+        }
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+extension HomeScreenViewController: SidebarViewDelegate {
+    func sidebarDidSelectRow(row: Row) {
+        blackScreen.isHidden = true
+        blackScreen.frame = self.view.bounds
+        UIView.animate(withDuration: 0.3) {
+            self.sidebarView.frame=CGRect(x: 0, y: 0, width: 0, height: self.sidebarView.frame.height)
+            }
+        switch row {
+        case .editProfile:
+            let vc=EditProfileVC()
+            self.navigationController?.pushViewController(vc, animated: true)
+        case .addTicket:
+            print("Add Ticket")
+        case .location:
+            print("Location")
+        case .contact:
+            print("Contact")
+        case .signOut:
+            print("Sign out")
+        case .none:
+            break
+            //        default:  //Default will never be executed
+            //            break
+        }
+}
 }
