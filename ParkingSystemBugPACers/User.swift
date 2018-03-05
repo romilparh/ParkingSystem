5e6433321a2347adb81d81441d5951b2998fc865
Romil <romilparhwal007@gmail.com>
2018-03-04 20:18:09 -0500

    Add User Class to have Users in Service

---
+++ ParkingSystemBugPACers/User.swift
@@ -0,0 +1,45 @@
+//
+//  User.swift
+//  ParkingSystemBugPACers
+//
+//  Created by Romil Parhwal on 2018-03-04.
+//  Copyright © 2018 ShadyBond. All rights reserved.
+//
+
+import Foundation
+
+class UserInfo{
+    var fullName: String!
+    var eMail: String!
+    var password: String!
+    
+    
+    init(){
+        self.fullName = ""
+        self.eMail = ""
+        self.password = ""
+    }
+    
+    func addUser(_ userName: String,_ eMail: String,_ passwordEntered: String) -> Bool{
+        if(isValidEmail(eMail)){
+            self.fullName = userName
+            self.eMail = eMail
+            self.password = passwordEntered
+            return true
+        } else {
+            print("E-Mail not valid or already registered")
+            return false
+        }
+    }
+    
+    func isValidEmail(_ testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+.com"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    
+}
+
+var userDatabase = [UserInfo]()
